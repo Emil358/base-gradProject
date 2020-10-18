9fb7abe238e5a37444f7fef6e92d9a8629d12ff3
Emil358 <58589956+Emil358@users.noreply.github.com>
2020-10-18 21:57:38 +0300

    full configuration with HMR

--- bin/dev.js
+++ bin/dev.js
@@ -1,32 +1,93 @@
 const nodemon = require('nodemon');
 const path = require('path');
+const webpack = require('webpack');
+const [clientConfig, serverConfig] = require('../webpack.config');
+const WebpackDevMiddleware = require('webpack-dev-middleware');
+const WebpackHotMiddleware = require('webpack-hot-middleware');
+const express = require('express');
 
-let run = require('parallel-webpack').run,
-    configPath = require.resolve('../webpack.config.js'),
-    options = {
-      watch: true,
-      maxRetries: 1,
-      stats: true,
-      maxConcurrentWorkers: 2
-    };
+const hmrServer = express();
+const clientCompiler = webpack(clientConfig);
 
-function notify() {
+hmrServer.use(WebpackDevMiddleware(clientCompiler, {
+  publicPath: clientConfig.output.publicPath,
+  serverSideRender: true,
+  noInfo: true,
+  watchOptions: {
+    ignore: /dist/,
+  },
+  writeToDisk: true,
+  stats: 'errors-only'
+}));
+
+hmrServer.use(WebpackHotMiddleware(clientCompiler, {
+  path: '/static/__webpack_hmr',
+}));
+
+hmrServer.listen(3001, () => {
+  console.log('HMR server succesful started {-1}')
+})
+
+const compiler = webpack(serverConfig, (err, stats) => {
+  if (err) {
+    console.log('Compilation failed {0}', err);
+    return;
+  }
+
+  console.log(stats.toString({
+    chunks: false,  // Makes the build much quieter
+    colors: true    // Shows colors in the console
+  }));
+});
+
+compiler.run((err) => {
+  if(err) {
+    console.log('Compilation failed {1}: ', err);
+  }
+
+  compiler.watch({}, (err) => {
+    if(err) {
+      console.log('Compilation failed {2}: ', err);
+    }
+
+    console.log('Compilation was succesfull {3}');
+  });
+
   nodemon({
     script: path.resolve(__dirname, '../dist/server/server.js'),
     watch: [
       path.resolve(__dirname, '../dist/server'),
       path.resolve(__dirname, '../dist/client'),
     ],
-  });
+  })
+});
 
-  nodemon.on('start', function () {
-    console.log('App has started');
-  }).on('quit', function () {
-    console.log('App has quit');
-    process.exit();
-  }).on('restart', function (files) {
-    console.log('App restarted due to: ', files);
-  });
-}
+// let run = require('parallel-webpack').run,
+//     configPath = require.resolve('../webpack.config.js'),
+//     options = {
+//       watch: true,
+//       maxRetries: 1,
+//       stats: true,
+//       maxConcurrentWorkers: 2
+//     };
 
-run(configPath, options, notify);
+// function notify() {
+//   nodemon({
+//     script: path.resolve(__dirname, '../dist/server/server.js'),
+//     watch: [
+//       path.resolve(__dirname, '../dist/server'),
+//       path.resolve(__dirname, '../dist/client'),
+//     ],
+//   });
+
+//   nodemon.on('start', function () {
+//     console.log('App has started');
+//   }).on('quit', function () {
+//     console.log('App has quit');
+//     process.exit();
+//   }).on('restart', function (files) {
+//     console.log('App restarted due to: ', files);
+//   });
+// }
+
+// run(configPath, options, notify);
--- src/shared/header.jsx
+++ src/shared/header.jsx
@@ -1,9 +1,12 @@
-import * as React from 'react'
+import * as React from 'react';
+import { hot } from 'react-hot-loader/root'
 
-export function Header () {
+function HeaderComponent () {
   return (
     <header>
       <h1>Hello world</h1>
     </header>
   );
 }
+
+export const Header = hot(HeaderComponent);
