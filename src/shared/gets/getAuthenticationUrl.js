2a0322aeb5bd4d6268f828d83503740f81e5db54
Emil358 <58589956+Emil358@users.noreply.github.com>
2020-11-15 01:26:47 +0300

    running on the server. attempt 4

--- src/shared/gets/getAuthenticationUrl.js
+++ src/shared/gets/getAuthenticationUrl.js
@@ -4,7 +4,7 @@ export const getAuthUrl = () => {
   const unsplash = new Unsplash({
     accessKey: 'HmESHW-LSmwiDl4Gs3MOm-Qlksijb1XVrTIN4W7nJCc',
     secret: 'wMv3YV1LWZtyzVo46q63L_yBS2GN-Ceqa7OP6WZYgOs',
-    callbackUrl: 'http://localhost:3000/auth'
+    callbackUrl: 'http://faiz-emil.ru/auth'
   })
 
   const authenticationUrl = unsplash.auth.getAuthenticationUrl([
